d94b6d342a7e431280f790798a96c9951901fcef
Lisa Ngo <lisa.t.ngo@gmail.com>
2017-06-13 10:55:32 -0400

    Added Celtic Cross layout.
    Added Celtic Cross layout (10 cards).

--- tarot.py
+++ tarot.py
@@ -44,7 +44,18 @@ class deck:
             print("The Present is represented by: the "+self.usedCards[1]+". Its meaning is: \n"+self.interpretation.card(self.usedCards[1])+".")
             print("The Future is represented by: the "+self.usedCards[2]+". Its meaning is: \n"+self.interpretation.card(self.usedCards[2])+".")
         elif type == "multipleCards":
-            print("In progress.")
+            print("There are 10 cards drawn in a Celtic Cross layout.")
+            print("The Present represents what is happening to the querent right now, and is represented by the "+self.usedCards[0]+". Its meaning is: \n"+self.interpretation.card(self.usedCards[0])+".")
+            print("The Challenge lays atop the Present card and represents the immediate problem or challenge facing the querent. It is represented by the "+self.usedCards[1]+". Its meaning is \n"+self.interpretation.card(self.usedCards[0])+".")
+            print("The Past stants to the left and represents the events leading up to the present situation. It is represented by the "+self.usedCards[2]+". Its meaning is: \n"+self.interpretation.card(self.usedCards[2])+".")
+            print("The Future stands to the right and represents what is likely to occur soon. It is represented by the "+self.usedCards[3]+". Its meaning is \n"+self.interpretation.card(self.usedCards[3])+".")
+            print("The Above card is above these cards, and reflects the querent's goal, aspiration or best outcome.  It is represented by the "+self.usedCards[4]+". Its meaning is \n"+self.interpretation.card(self.usedCards[4])+".")
+            print("The Below card is below these cards, and reflects what is written within the querent's subconscious, delving deeper into the core foundation. It is represented by the "+self.usedCards[4]+". Its meaning is \n"+self.interpretation.card(self.usedCards[4])+".")
+            print("These other cards are laid out to the right of the cross. It is represented by the "+self.usedCards[5]+". Its meaning is \n"+self.interpretation.card(self.usedCards[5])+".")
+            print("The Advice card takes into account everything happening within the querent's life and provides a recommedation. It is represented by the "+self.usedCards[6]+". Its meaning is \n"+self.interpretation.card(self.usedCards[6])+".")
+            print("The External Influences card highlights the people, energies, or events which will affect the outcome of the question. It is represented by the "+self.usedCards[7]+". Its meaning is \n"+self.interpretation.card(self.usedCards[7])+".")
+            print("The Hopes and Fears card represents the person's hopes and fears. This can be a complicated card to interpret. It is represented by the "+self.usedCards[8]+". Its meaning is \n"+self.interpretation.card(self.usedCards[8])+".")
+            print("The Outcome card represents where the situation is headed and if/how the issue will be resolved, if it is resolved. It is represented by the "+self.usedCards[9]+". Its meaning is \n"+self.interpretation.card(self.usedCards[9])+".")
         else:
             print("Your card is: the "+self.usedCards[0]+". Its meaning is: "+self.interpretation.card(self.usedCards[0]+"."))
 
